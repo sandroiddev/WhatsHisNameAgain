966b5b547056415458aac966e9b46de066c4609a
James Sanders <jsandersii@Jamess-MBP.attlocal.net>
2016-02-11 00:56:49 -0600

    Initial Commit
    App Theme created
    Started creating the shell of the app.

---
+++ MyApplication/app/src/main/java/sanstormsolutions/com/whatshisname/activities/MainActivity.java
@@ -0,0 +1,67 @@
+package sanstormsolutions.com.whatshisname.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import sanstormsolutions.com.whatshisname.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFab = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setupViews();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /***
+     * Method for setting up all views for this activity
+     */
+    private void setupViews() {
+
+        mFab = (FloatingActionButton) findViewById(R.id.activity_main_fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+}
---
+++ MyApplication/app/src/main/java/sanstormsolutions/com/whatshisname/models/People.java
@@ -0,0 +1,54 @@
+package sanstormsolutions.com.whatshisname.models;
+
+/**
+ * Created by jsandersii on 2/11/16.
+ * The primary use for this class is to act as the model for the 'Person' Node on Firebase.
+ */
+public class People {
+
+    //Vars
+    private String firstName = null; //First Name of the person
+    private String lastName = null; // Last Name of the person
+    private String birthday = null; // DOB of the person
+    private int zipCode; // Home Zip Code of the person
+
+    //Default Constructor for firebase object mapping.
+    public People(){
+
+    }
+
+
+    // Getters & Setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+}
